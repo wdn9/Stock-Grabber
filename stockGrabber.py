14523fedd048d7e80ef4299e161b8d53a48afddd
Wes Nicol <wdn9@txstate.edu>
2018-11-04 09:45:30 -0600

    got current index and change

---
+++ stockGrabber.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 31 10:31:14 2018
+
+@author: Wes Nicol
+"""
+
+# import libraries
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+#URL of the webpage being scraped
+url = 'https://finance.yahoo.com/quote/%5EDJI/'
+
+# query the website and return the html to the variable ‘page’
+page = urllib.request.urlopen(url)
+
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(page, 'html.parser')
+
+
+
+#grab the actual current market index
+marketIndex = soup.find_all('span',class_="Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)")
+
+
+# grab the amount of change in the current day
+dayChange = soup.find_all('span',class_="Trsdu(0.3s) Fw(500) Fz(14px) C($dataRed)")
+
+
+
+print(marketIndex)
+print(dayChange)
