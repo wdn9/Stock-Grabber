17d0fb6168b4a888db4ab6acc8b4db3034e0961a
Wes Nicol <wdn9@txstate.edu>
2018-11-05 17:11:23 -0600

    Now grabs from CNN

--- getIndex.py
+++ getIndex.py
@@ -10,75 +10,88 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-#URL of the webpage being scraped
-url = 'https://finance.yahoo.com/quote/%5EDJI/'
+# this function reutrns the current index of the DOW Jones (according to cnn) as a float
+def getDOWIndex():
+    url = 'https://money.cnn.com/data/markets/dow/'
 
-# query the website and return the html to the variable ‘page’
-page = urllib.request.urlopen(url)
+    # query the website and return the html to the variable ‘page’
+    page = urllib.request.urlopen(url)
+    
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page, 'html.parser')
+
+    marketIndexTag = soup.find('span',stream='last_599362')
+    
+    #grab the actual current market index
+    currentIndex = marketIndexTag.text
+    
+    
+    #remove comma from current index number
+    currentIndex = currentIndex.replace(',', '')
+    
+    #convert currentIndex to float
+    currentIndex = float(currentIndex)
+    
+    
+    return currentIndex
 
 
-# parse the html using beautiful soup and store in variable `soup`
-soup = BeautifulSoup(page, 'html.parser')
+
+
+def getDayChange():
+    url = 'https://money.cnn.com/data/markets/dow/'
+
+    # query the website and return the html to the variable ‘page’
+    page = urllib.request.urlopen(url)
+
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    # grab the amount of change in the current day
+    dayChangeTag = soup.find('span',class_='posData')
+    #if positive change isnt found, look for negitive one
+    if(dayChangeTag is None): 
+        dayChangeTag = soup.find('span',class_='negData')
+    
+    dayChangeText = dayChangeTag.text
+    
+    return float(dayChangeText)
+
+    
+def getDayChangePercentage():
+    url = 'https://money.cnn.com/data/markets/dow/'
+
+    # query the website and return the html to the variable ‘page’
+    page = urllib.request.urlopen(url)
+    
+    # parse the html using beautiful soup and store in variable `soup`
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # grab the amount of change in the current day
+    dayChangeTag = soup.find_all('span',class_='posData')
+    
+    # index 1 is used because thats where the data is stored in the html document
+    dayChangeText = dayChangeTag[1].text
+
+
+    #strip '(',')' & '%' from percentage
+    dayChangeText = dayChangeText.replace(')', '')
+    dayChangeText = dayChangeText.replace('(', '')
+    dayChangeText = dayChangeText.replace('%', '')
+
+    return float(dayChangeText)
 
 '''
-print(soup.prettify())
-print()
-print()
+print('DOWIndex:\t\t' + str(getDOWIndex()))
+print('Current Day Channge:\t' + str(getDayChange()))
+print('Precentage Change:\t' + str(getDayChangePercentage()) + '%')
 '''
 
-#grab the actual current market index
-marketIndexTag = soup.find('span',
-                           class_="Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)")
 
-
-# grab the amount of change in the current day
-dayChangeTag = soup.find('span',
-                         class_="Trsdu(0.3s) Fw(500) Fz(14px) C($dataRed)")
-
-
-
-currentIndex = marketIndexTag.text
-
-#remove comma from current index number
-currentIndex = currentIndex.replace(',', '')
-
-#convert currentIndex to float
-currentIndex = float(currentIndex)
-print(currentIndex)
-print()
-
-
-
-dayChangeText = dayChangeTag.text
-
-
-#seperate day change from day change percentage
-dayChangeList = dayChangeText.split()
-
-#strip '(',')' & '%' from percentage
-dayChangeList[1] = dayChangeList[1].replace(')', '')
-dayChangeList[1] = dayChangeList[1].replace('(', '')
-dayChangeList[1] = dayChangeList[1].replace('%', '')
-
-dayChange = float(dayChangeList[0])
-dayChangePercentage = float(dayChangeList[1])
-
-
-
-print(dayChange)
-print()
-
-print(dayChangePercentage)
-print()
-
-
-
-
-
-
-
-
-
+'''
+while(True):
+'''
+    
 
 
 
