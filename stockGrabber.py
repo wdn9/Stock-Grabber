bb7359fd6630d19a353fbf29a4564b7bbd9578ef
Caleb Kress <calebkress@gmail.com>
2019-02-17 19:40:06 -0600

    clarified comments

--- stockGrabber.py
+++ stockGrabber.py
@@ -14,7 +14,7 @@ from BeautifulSoup import BeautifulSoup
 #URL of the webpage being scraped
 url = 'https://finance.yahoo.com/quote/%5EDJI/'
 
-# query the website and return the html to the variable ‘page’
+# query the website and return the html
 response = requests.get(url)
 html = response.content
 
@@ -25,12 +25,12 @@ soup = BeautifulSoup(html)
 marketIndexTag = soup.find('span', {'class': 'Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)'}).text.replace(',', '')
 print(marketIndexTag)
 
-# grab the amount of change in the current day
+# grab the amount of change in the current day whether up or down, sanitize inputs to just '+float'
 if soup.find('span', {'class': 'Trsdu(0.3s) Fw(500) Fz(14px) C($dataRed)'}) == None:
     dayChangeTag = soup.find('span', {'class': 'Trsdu(0.3s) Fw(500) Fz(14px) C($dataGreen)'}).text.encode('utf-8').replace('(', '').replace(')', '').replace('+', '').replace('%', '')
 else:
     dayChangeTag = soup.find('span', {'class': 'Trsdu(0.3s) Fw(500) Fz(14px) C($dataRed)'}).text.encode('utf-8').replace('(', '').replace(')', '').replace('+', '').replace('%', '')
 
-#split day change from day change percentage
+#split day change from day change percentage, return list of strs
 dayChangeList = dayChangeTag.split(' ')
 print(dayChangeList)
